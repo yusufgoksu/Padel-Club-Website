0293e4cde503bda1138a402f5419a951488992f0
yusufgoksu <goksuy@mef.edu.tr>
2025-03-26 18:56:33 +0000

    updated

--- src/main/kotlin/services/courtSevices.kt
+++ src/main/kotlin/services/courtSevices.kt
@@ -14,7 +14,7 @@ object CourtServices {
 
     fun getUsers(): List<User> = CourtsDataMem.users.values.toList()
 
-    fun getUser(uid: String): User? = CourtsDataMem.users[uid]  // ✅ Eklendi
+
 
     fun addClub(name: String, ownerUid: String): Club {
         require(CourtsDataMem.users.containsKey(ownerUid)) { "Owner UID not found" }
@@ -25,7 +25,7 @@ object CourtServices {
 
     fun getClubs(): List<Club> = CourtsDataMem.clubs.values.toList()
 
-    fun getClub(cid: String): Club? = CourtsDataMem.clubs[cid]  // ✅ Eklendi
+
 
     fun addCourt(name: String, clubId: String): Court {
         require(CourtsDataMem.clubs.containsKey(clubId)) { "Club ID not found" }
@@ -36,7 +36,7 @@ object CourtServices {
 
     fun getCourts(): List<Court> = CourtsDataMem.courts.values.toList()
 
-    fun getCourt(crid: String): Court? = CourtsDataMem.courts[crid]  // ✅ Eklendi
+
 
     fun addRental(clubId: String, courtId: String, userId: String, startTime: String, duration: Int): Rental {
         require(CourtsDataMem.clubs.containsKey(clubId)) { "Club ID not found" }
@@ -50,5 +50,5 @@ object CourtServices {
 
     fun getRentals(): List<Rental> = CourtsDataMem.rentals.values.toList()
 
-    fun getRental(rid: String): Rental? = CourtsDataMem.rentals[rid]  // ✅ Eklendi
+
 }
